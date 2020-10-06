0bb6f09b4e0c1edac582b92094d0f0e5be0b6212
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-10-06 12:30:16 +0700

    Tạo dự án mới, cấu hình trong properties

---
+++ src/main/java/com/codegym/DemoDeploy0610Application.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeploy0610Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDeploy0610Application.class, args);
+    }
+
+}
